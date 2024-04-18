f22f60f47add8795f545f8c26aece4cabbb33f96
Eyal <eyalbenatan@gmail.com>
2024-04-18 16:38:20 +0300

    Fixed a bug after strtol, removed commented out code

--- app/parser.c
+++ app/parser.c
@@ -2,38 +2,6 @@
 
 #include "parser.h"
 
-/*
-static bool isDigit(char c) {
-    return c >= '0' && c <= '9';
-} */
-/*
-static bool isEmptyString(char* c) {
-    return *c == '-' && *(c + 1) == '1';
-}
-*/
-
-/**
- * Like atoi, except looks for \r\n instead of \0.
- * Expects only to recieve an unsigned int.
-*/
-/*
-static int convert_to_int(char** c) {
-    unsigned int value = 0;
-    while(**c != '\r') {
-        if (!isDigit(**c)) {
-            return -1;
-        }
-
-        value *= 10;
-        value += (int)(**c - '0');
-
-        (*c)++;
-    }
-    *c += 2; // Skip \r\n
-    return value;
-}
-
-*/
 static RespData* parse_resp_arr(char** c) {
     RespData* resp = malloc(sizeof(*resp));
     if (!resp) {
@@ -45,18 +13,12 @@ static RespData* parse_resp_arr(char** c) {
 
     int itemCount = (int) strtol(*c, c, 10);
     
-    /*if (itemCount == -1) {
-       printf("Couldn't convert length to number. original string was: %s", ((*c) - 2)); 
-       free(resp->as.arr);
-       free(resp);
-       return NULL;
-    } */
 
+    *c += 2; //Skipping the "\r\n"
     if (itemCount == -1) {
         // Null array
         resp->as.arr->length = -1;
         resp->as.arr->values = NULL;
-        *c += 2; // Skipping \r\n
         return resp;
     }
 
@@ -80,8 +42,8 @@ static RespData* parse_resp_blk_string(char** c) {
     resp->as.blk_str = (BlkStr*) malloc(sizeof(BlkStr));
 
     int str_len = (int) strtol(*c, c, 10);
+    *c += 2; //Skipping the "\r\n"
     if (str_len == -1) {
-        *c += 2; //Skipping the "\r\n"
         resp->as.blk_str->length = 0;
         resp->as.blk_str->chars = NULL;
         return resp;
@@ -109,25 +71,10 @@ static RespData* parse_resp_integer(char** c) {
 
     resp->type = RESP_INTEGER;
 
-    // bool isNegative = (**c == '-');
-    // if (**c == '+' || **c == '-') {
-    //     (*c)++;
-    // }
 
     resp->as.integer = (int) strtol(*c, c, 10);
-    /*
-    if (resp->as.integer == -1) {
-         // convert_to_int returns a positive value, so it failed here
-        printf("Couldn't convert string to number. original string was: %s", ((*c) - 2)); 
-        free(resp);
-        return NULL;
-    }
+    *c += 2; //Skipping the "\r\n"
     
-    if (isNegative) {
-        resp->as.integer = (-1)*AS_INTEGER(resp);
-    }
-    */
-
     return resp;
 }
 
@@ -150,6 +97,7 @@ static RespData* parse_resp_bool(char** c) {
 
     return resp;
 }
+
 RespData* parse_resp_data(char** input) { 
     char type = **input;
     (*input)++;
@@ -174,100 +122,6 @@ RespData* parse_resp_data(char** input) {
     }
 }
 
-/**
- * Returns the new capacity of dest
-*/
-/*
-static void append_string(char** dest, const char* src, int *dest_capacity) {
-    int src_len = strlen(src);
-    int dest_len;
-    if (*dest == NULL) {
-        int new_len = src_len + 1;
-        *dest = malloc(sizeof(char) * (new_len));
-        strcpy(*dest, src);
-        return; 
-    } 
-    dest_len = strlen(*dest);
-    if (*dest_capacity > dest_len + src_len) {
-        strcat(*dest, src);
-        return;
-    }
-    int new_capacity = (2 * (*dest_capacity) > (src_len + dest_len)) ? 2* (*dest_capacity) : (dest_len + src_len + 1);
-    *dest_capacity = new_capacity;
-    *dest = realloc(*dest, new_capacity * sizeof(char));
-    strcat(*dest, src);
-}
-*/
-/*
-char *convert_data_to_blk(RespData* input) {
-    // We use a defulat capacity of 64 since its enough most of the time
-    int capacity = 64;
-    char *result = malloc(capacity * sizeof(char));
-    result[0] = '\0';
-    resp_to_blk(input, &result, &capacity);
-    return result;
-}
-*/
-/*
-char* convert_blk_data_to_str(RespData* input) {
-    if (input->type != RESP_BULK_STRING) {
-        return "";
-    }
-    int inputLength = AS_BLK_STR(input)->length;
-    if (AS_BLK_STR(input)->chars == NULL) {
-        return "$-1\r\n";
-    }
-
-    char response[512];
-    char *encodedString = response;
-
-    sprintf(encodedString, "$%d\r\n", inputLength);
-    strcat(encodedString, AS_BLK_STR(input)->chars);
-    strcat(encodedString, "\r\n");
-
-    return encodedString;
-
-}
-*/
-/*
-static void resp_to_blk(RespData* input, char** result_string, int *dest_capacity) {
-    switch (input->type) {
-        case RESP_SIMPLE_ERROR:
-        case RESP_SIMPLE_STRING: 
-            append_string(result_string, "+", dest_capacity);
-            append_string(result_string, AS_SIMPLE_STR(input), dest_capacity);
-            append_string(result_string, "\r\n", dest_capacity);
-            break;
-        case RESP_INTEGER: {
-            char int_str[20] = {0};
-            sprintf(int_str, ":%d\r\n", AS_INTEGER(input));
-            append_string(result_string, int_str, dest_capacity);
-            break;
-        }
-        case RESP_BOOL: 
-            if (AS_BOOL(input)) {
-                append_string(result_string, "#t\r\n", dest_capacity);
-            } else {
-                append_string(result_string, "#f\r\n", dest_capacity);
-            }
-            break;
-        case RESP_BULK_STRING:
-            append_string(result_string, convert_blk_data_to_str(input), dest_capacity);
-            break;
-        case RESP_ARRAY: {
-            char arr_length_str[20] = {0};
-            sprintf(arr_length_str, "$%d\r\n", AS_ARR(input)->length);
-            append_string(result_string, arr_length_str, dest_capacity);
-            for (int i = 0; i < AS_ARR(input)->length; i++) {
-                resp_to_blk(AS_ARR(input)->values[i], result_string, dest_capacity);
-            }
-            break;
-        }
-        default:
-            break;
-    }
-}
-*/
 
 char *encode_resp_simple_string(char *simple_str) {
     char *encoded_ptr;
